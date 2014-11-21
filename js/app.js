193b67960a41f3287a3cf016b157d8c2d03462c6
Richard Martin <richardgeorgemartin@gmail.com>
2014-11-21 18:50:21 -0500

    Included genres

--- js/app.js
+++ js/app.js
@@ -44,6 +44,7 @@ App.Book = DS.Model.extend({
   review: DS.attr(),
   rating: DS.attr('number'),
   amazon_id: DS.attr(),
+  genre: DS.belongsTo('genre', {async: true}),
   url: function() {
     return 'http://www.amazon.com/gp/product/'+this.get('amazon_id')+'/adamfortuna_20';
   }.property('amazon_id'),
@@ -52,7 +53,7 @@ App.Book = DS.Model.extend({
   }.property('amazon_id')
 });
 
-// Define Book FIXTURE to house data
+// Define Book FIXTURE to house Book data
 
 App.Book.FIXTURES = [
   {
@@ -61,7 +62,8 @@ App.Book.FIXTURES = [
     author: 'Seymour A. Papert',
     review: 'Although this book focuses on the cognitive advantages to having children use technology from an early age, it is also an in depth look at how people can learn for themseves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.',
     rating: 5,
-    amazon_id: '0465046746'
+    amazon_id: '0465046746',
+    genre: 3
   },
   {
     id: 2,
@@ -69,7 +71,8 @@ App.Book.FIXTURES = [
     author: 'Dan Simmons',
     review: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude to the larger storyline. The scope of the story is ambitious - spanning time, planets religion and love.",
     rating: 5,
-    amazon_id: '0553283685'
+    amazon_id: '0553283685',
+    genre: 1
   },
   {
     id: 3,
@@ -77,6 +80,35 @@ App.Book.FIXTURES = [
     author: 'Leander Kahney',
     review: "Even though I respect Ive, I felt this biography only hit skin deep. It went over all the major events in his life, his passion for design, awards he achieved -- but that's really it. I dont't feel I know him anymore than before reading this.",
     rating: 2,
-    amazon_id: '159184617X'
+    amazon_id: '159184617X',
+    genre: 3
   }
 ];
+
+// Define Ember Data Object for Genre
+
+App.Genre = DS.Model.extend({
+  name: DS.attr(),
+  books: DS.hasMany('book')
+});
+
+// Define FIXTURE to house Genre data
+
+App.Genre.FIXTURES = [
+  {
+    id: 1,
+    name: 'Science Fiction',
+    books: [2]
+  },
+  {
+    id: 2,
+    name: 'Fiction'
+  },
+  {
+    id: 3,
+    name: 'Non-Fiction',
+    books: [1,3]
+  }
+];
+
+
